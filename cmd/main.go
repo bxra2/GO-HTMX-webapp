ce23b7eaec618f20ea3bc89a5b54d5644f3eae9c
baraaashraf <baraaashraf@protonmail.com>
2024-06-03 14:33:27 +0800

    go backend

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,82 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"sync"
 	"time"
 
 	"github.com/bxra2/GO-HTMX-webapp/internal/hardware"
+	"nhooyr.io/websocket"
 )
+
+type server struct {
+	subscriberMessageBuffer int
+	mux                     http.ServeMux
+	subscribersMutex        sync.Mutex
+	subscribers             map[*subscriber]struct{}
+}
+
+type subscriber struct {
+	msgs chan []byte
+}
+
+func NewServer() *server {
+	s := &server{
+		subscriberMessageBuffer: 10,
+		subscribers:             make(map[*subscriber]struct{})}
+
+	s.mux.Handle("/", http.FileServer(http.Dir("./htmx")))
+	s.mux.HandleFunc("/ws", s.subscribeHandler)
+	return s
+}
+
+func (s *server) subscribeHandler(w http.ResponseWriter, r *http.Request) {
+	err := s.subscribe(r.Context(), w, r)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (s *server) subscribe(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	var c *websocket.Conn
+	subscriber := &subscriber{
+		msgs: make(chan []byte, s.subscriberMessageBuffer),
+	}
+	s.addSubscriber(subscriber)
+
+	c, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		return err
+	}
+	defer c.CloseNow()
+
+	ctx = c.CloseRead(ctx)
+	for {
+		select {
+		case msg := <-subscriber.msgs:
+			ctx, cancel := context.WithTimeout(ctx, time.Second*5)
+			defer cancel()
+			err := c.Write(ctx, websocket.MessageText, msg)
+			if err != nil {
+				return err
+			}
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
+
+func (s *server) addSubscriber(subscriber *subscriber) {
+	s.subscribersMutex.Lock()
+	s.subscribers[subscriber] = struct{}{}
+	s.subscribersMutex.Unlock()
+	fmt.Println("added subscriber", subscriber)
+
+}
 
 func main() {
 	fmt.Println("Starting System Mointor...")
@@ -37,5 +108,9 @@ func main() {
 		}
 	}()
 
-	time.Sleep(3 * time.Minute)
+	err := http.ListenAndServe(":8080", &NewServer().mux)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,16 @@ module github.com/bxra2/GO-HTMX-webapp
 go 1.21.10
 
 require (
+	github.com/shirou/gopsutil v2.21.11+incompatible
+	golang.org/x/net v0.25.0
+)
+
+require (
 	github.com/go-ole/go-ole v1.3.0 // indirect
-	github.com/shirou/gopsutil v2.21.11+incompatible // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.14 // indirect
 	github.com/tklauser/numcpus v0.8.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/sys v0.20.0 // indirect
+	nhooyr.io/websocket v1.8.11 // indirect
 )
